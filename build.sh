5a4a06516ddb32b188f34c1d15e022dfa7d6220d
Harold <power0721@gmail.com>
2022-06-11 19:38:05 +0800

    dependency

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 mvn clean package
+mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
 sudo docker build -t springio/gs-spring-boot-docker .
---
+++ run.sh
@@ -0,0 +1,1 @@
+sudo docker run -p 8080:8080 -t springio/gs-spring-boot-docker
